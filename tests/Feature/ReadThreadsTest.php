f276b7e330e17e4073272fcae925cffcbb8f7208
Mukotso <kelvinmukotso@gmail.com>
2022-02-13 16:14:02 +0300

    v-14  a user can filter threads by username

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -54,4 +54,15 @@ class ReadThreadsTest extends TestCase
             ->assertDontSee($threadNotInChannel);
 
 }
+
+public function a_user_can_filter_threads_by_any_username(){
+        $this->signIn(create('App\User', ['name'=>'johnDoe']));
+
+        $threadByJohn = create ('App\Thread',['user_id'=>auth()->id()]);
+        $threadNotByJohn =create('App\Thread');
+
+        $this->get('threads?by=JohnDoe')
+            ->assertSee($threadByJohn ->title)
+            ->assertDontSee($threadNotByJohn->title);
+}
 }
