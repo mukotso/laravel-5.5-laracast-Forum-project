8e17f0bd02bf3bdc6967ec1528f3135b2a53940d
Mukotso <kelvinmukotso@gmail.com>
2022-02-24 06:03:58 +0300

    v-29 flash messsages withnVue

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -44,7 +44,8 @@ class ThreadsController extends Controller
             'body' => request('body')
         ]);
 
-        return redirect($thread->path());
+        return redirect($thread->path())
+            ->with('flash', 'Your thread has been published');
     }
 
     public function show( $channelId,Thread $thread)
