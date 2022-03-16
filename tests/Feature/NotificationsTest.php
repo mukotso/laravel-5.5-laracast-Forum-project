76f2a6efff31857582b31062f1175ec9f53571b1
mukotso <kelvinmukotso@gmail.com>
2022-03-16 07:54:26 +0300

    v-44 test re-factoring

--- tests/Feature/NotificationsTest.php
+++ tests/Feature/NotificationsTest.php
@@ -3,15 +3,21 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Notifications\DatabaseNotification;
 use Tests\TestCase;
 
 class NotificationsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public function setUp(){
+        parent::setUp();
+        $this->signIn();
+    }
+
     function a_notification_is_prepared_when_a_subscribed_thread_receives_a_new_reply_that_is_not_by_the_current_user()
     {
-        $this->signIn();
+
         $thread = create('App\thread')->subscribe();
         $this->assertCount(0, auth()->user()->notifications);
         $thread->addReply([
@@ -29,12 +35,14 @@ class NotificationsTest extends TestCase
 
     function a_user_can_fetch_their_unread_notifications()
     {
-        $this->signIn();
-        $thread = create('App\thread')->subscribe();
-        $thread->addReply([
-            'user_id' => create('App\User')->id,
-            'body' => 'some reply here'
-        ]);
+
+        create(DatabaseNotification::class);
+
+//        $thread = create('App\thread')->subscribe();
+//        $thread->addReply([
+//            'user_id' => create('App\User')->id,
+//            'body' => 'some reply here'
+//        ]);
 
         $response = $this->getJson('/profiles/' . auth()->user()->name . '/notifications')->json();
         $this->assertCount(1, $response);
@@ -42,12 +50,14 @@ class NotificationsTest extends TestCase
 
     function a_user_can_mark_a_notification_as_read()
     {
-        $this->signIn();
-        $thread = create('App\thread')->subscribe();
-        $thread->addReply([
-            'user_id' => auth()->id,
-            'body' => 'some reply here'
-        ]);
+
+        create(DatabaseNotification::class);
+//
+//        $thread = create('App\thread')->subscribe();
+//        $thread->addReply([
+//            'user_id' => auth()->id,
+//            'body' => 'some reply here'
+//        ]);
         $user = auth()->user();
         $this->assertCount(1, $user->unreadNotifications);
 
